507248a609c7e5867f0b53b989596af4ceb980d4
jonathanlal <jonathanmlaliberte@gmail.com>
2023-03-05 20:44:51 +0100

    fix tspaths issue with declaration files

--- packages/frostbyte/rollup.config.js
+++ packages/frostbyte/rollup.config.js
@@ -4,6 +4,7 @@ import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import dts from 'rollup-plugin-dts';
+import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
 import { readFileSync } from 'fs';
 
 const packageJson = JSON.parse(
@@ -37,6 +38,6 @@ export default [
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     external: [/\.css$/],
-    plugins: [dts()],
+    plugins: [dts(), tsConfigPaths()],
   },
 ];
